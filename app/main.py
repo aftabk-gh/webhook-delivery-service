a193d024b7eb091ab51c60666755f27289f528b8
Aftab <aftab76ahmad76@gmail.com>
2026-04-14 14:11:40 +0500

    add pydantic-settings

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI, status
 from pydantic import BaseModel
 
+from app.config import get_settings
+
 app = FastAPI()
+settings = get_settings()
 
 
 class HealthResponse(BaseModel):
     status: str
+    app_name: str
 
 
 @app.get("/ping", response_model=HealthResponse, status_code=status.HTTP_200_OK)
 async def health_check() -> HealthResponse:
-    return HealthResponse(status="ok")
+    return HealthResponse(status="ok", app_name=settings.app_name)
